daa19ac33f4c3efcf1f0457c4d4d9ab8933fb3f2
Роман Иванов <romaiv99@gmail.com>
2023-08-14 22:45:56 +0300

    Add Tests

---
+++ TestProject/PalletTryAddBoxTests.cs
@@ -0,0 +1,30 @@
+using Test.Models;
+
+namespace TestProject;
+
+public class PalletTryAddBoxTests
+{
+    private Pallet _pallet = new Pallet(0, 10, 0.5f, 10);
+
+    private Box _boxInPalletTrue = new Box(0, 10, 10, 10, 50, "Картошка");
+    private Box _boxInPalletFalse = new Box(1, 10, 10, 20, 250, "Гвозди");
+    
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void TryAddBox_ReturnTrue()
+    {
+        bool result = _pallet.TryAddBox(_boxInPalletTrue);
+        Assert.IsTrue(result, "Коробка должна была добавиться");
+    }
+    
+    [Test]
+    public void TryAddBox_ReturnFalse()
+    {
+        bool result = _pallet.TryAddBox(_boxInPalletFalse);
+        Assert.IsFalse(result, "Коробка не должна была добавляться");
+    }
+}
